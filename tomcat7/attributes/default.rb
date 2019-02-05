68b03247b6cae9965562023e2b9ef276d39bb21a
janakdevops <devopstechlab@gmail.com>
2019-02-05 18:58:05 +0530

    Chef with Tomcat7 Setup

---
+++ tomcat7/attributes/default.rb
@@ -0,0 +1,4 @@
+node.default['tomcat7']['s3_ezcac_common_package_path'] = "s3://ezcac/common"
+node.default['tomcat7']['s3_apache-tomcat_source_package_dir'] = "source"
+node.default['tomcat7']['s3_apache-tomcat_source_package'] = "apache-tomcat-7.0.50.tar.gz"
+node.default['tomcat7']['tomcat_dirname'] = "apache-tomcat-7.0.50"
---
+++ tomcat7/recipes/default.rb
@@ -0,0 +1,73 @@
+# Cookbook Name:: tomcat7
+# Recipe:: tomcat
+
+include_recipe "s3cmd"
+include_recipe "jre7"
+
+min_perm_size = "%.0f" % "#{node['memory']['total'][0..-3].to_i / 1024 * 0.10 }"
+max_perm_size = "%.0f" % "#{node['memory']['total'][0..-3].to_i / 1024 * 0.50 }"
+min_app_memory = "%.0f" % "#{node['memory']['total'][0..-3].to_i / 1024 * 0.40 }"
+max_app_memory = "%.0f" % "#{node['memory']['total'][0..-3].to_i / 1024 * 0.70 }"
+
+execute "get_apache_tomcat_source_package" do
+  user "ubuntu"
+  cwd  "/usr/local"
+  command "sudo s3cmd get #{node['tomcat7']['s3_ezcac_common_package_path']}/#{node['tomcat7']['s3_apache-tomcat_source_package_dir']}/#{node['tomcat7']['s3_apache-tomcat_source_package']} -c /home/ubuntu/.s3cfg"
+  not_if { File.exists?("/usr/local/#{node['tomcat7']['s3_apache-tomcat_source_package']}") }
+end
+
+directory "/usr/local/#{node['tomcat7']['tomcat_dirname']}" do
+  recursive true
+  owner "ubuntu"
+  group "ubuntu"
+  not_if { File.directory?("/usr/local/#{node['tomcat7']['tomcat_dirname']}") }
+end
+
+execute "extract_apache_tomcat_source_package" do
+  user "ubuntu"
+  group "ubuntu"
+  cwd  "/usr/local"
+  command "tar xzf #{node['tomcat7']['s3_apache-tomcat_source_package']}  -C #{node['tomcat7']['tomcat_dirname']}/ --strip-components=1"
+  not_if { File.exists?("/usr/local/#{node['tomcat7']['tomcat_dirname']}/bin/startup.sh") }
+end
+
+template "/usr/local/#{node['tomcat7']['tomcat_dirname']}/bin/catalina.sh" do
+  source "catalina.sh.erb"
+  owner "ubuntu"
+  group "ubuntu"
+  mode  "0755"
+  variables({
+        :min_perm_size => min_perm_size,
+        :max_perm_size => max_perm_size,
+        :min_app_memory => min_app_memory,
+        :max_app_memory => max_app_memory
+  })
+end
+
+template "/usr/local/#{node['tomcat7']['tomcat_dirname']}/conf/tomcat-users.xml" do
+  source  "tomcat-users.xml.erb"
+  owner "ubuntu"
+  group "ubuntu"
+  mode  "0600"
+end
+
+template "/etc/init.d/tomcat" do
+  source  "tomcat.erb"
+  owner "root"
+  group "root"
+  mode  "0777"
+end
+
+execute "add_tomcat_service" do
+  action :run
+  user "root"
+  group "root"
+  cwd  "/etc/init.d"
+  command "update-rc.d tomcat defaults"
+end
+
+service "tomcat" do
+  action [:enable, :start]
+  supports :status => true, :start => true, :stop => true, :restart => true
+  subscribes :restart, "template[/etc/init.d/tomcat]", :immediately
+end
